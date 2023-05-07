0bbc6f71cc0f7e7eca3936529f8893185a63a435
Prince22378 <prince22378@iiitd.ac.in>
2023-05-07 09:17:22 +0530

    done by taking user input

--- sum.py
+++ sum.py
@@ -1,3 +1,8 @@
-a = 5
-b = 7
-print("sum is",a+b)
+# a = 5
+# b = 7
+# print("sum is",a+b)
+
+# we will do the sum by taking input from users:
+a = int(input("Enter value of a : "))
+b = int(input("Enter value of b : "))
+print("Sum is",a+b);
